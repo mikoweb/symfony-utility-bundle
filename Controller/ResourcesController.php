235b524c25824d043b73ff7cd84367a7772fe0be
Rafał Mikołajun <rafal@mikoweb.pl>
2015-03-27 12:14:18 +0000

    każdy motyw graficzny posiada niezależny arkusz stylów

--- Controller/ResourcesController.php
+++ Controller/ResourcesController.php
@@ -30,31 +30,38 @@ class ResourcesController extends Controller
     /**
      * Arkusz CSS dla określonego modułu WebUI
      * @param $name nazwa modułu
+     * @param $theme nazwa motywu graficznego
      * @return Response
      * @throws NotFoundHttpException
      */
-    public function webuiCssAction($name)
+    public function webuiCssAction($name, $theme)
     {
+        try {
+            $this->get('liip_theme.active_theme')->setName($theme);
+        } catch (\Exception $e) {
+            throw new NotFoundHttpException('Theme not found.');
+        }
+
         $name = str_replace('|', '/', $name);
         $doc = new HtmlDocument();
         $params = $this->container->getParameter("vsymfo_core.document");
         $path = $this->container->get('app_path');
         $utility = HtmlDocumentSetup::getUtility($path, $this->container->get('kernel')->getEnvironment(), $params, array(
-                'less_import_dirs' => array(
-                    $path->absolute("web_theme") . $path::WEB_WEBUI . '/' => $path->url('web_theme')  . '/',
-                    $path->absolute("webui") . '/' => $path->url('web_theme')  . '/',
-                    $path->absolute("web_theme") . $path::WEB_RESOURCES  . '/' => $path->url('web_theme')  . '/',
-                    $path->absolute("web_resources") . '/' => $path->url('web_theme')  . '/',
-                ),
-                'less_globasls' => array(
-                    'path-base'      => '"' . $path->url('base') . '"',
-                    'path-theme'     => '"' . $path->url('web_theme') . '"',
-                    'path-resources' => '"' . $path->url('web_resources') . '"',
-                    'path-webui'     => '"' . $path->url('webui') . '"',
-                    'path-webui-engine' => '"' . $path->url('webui_engine') . '"',
-                    'domain-path' => '""',
-                ),
-            ));
+            'less_import_dirs' => array(
+                $path->absolute("web_theme") . $path::WEB_WEBUI . '/' => $path->url('web_theme')  . '/',
+                $path->absolute("webui") . '/' => $path->url('web_theme')  . '/',
+                $path->absolute("web_theme") . $path::WEB_RESOURCES  . '/' => $path->url('web_theme')  . '/',
+                $path->absolute("web_resources") . '/' => $path->url('web_theme')  . '/',
+            ),
+            'less_globasls' => array(
+                'path-base'      => '"' . $path->url('base') . '"',
+                'path-theme'     => '"' . $path->url('web_theme') . '"',
+                'path-resources' => '"' . $path->url('web_resources') . '"',
+                'path-webui'     => '"' . $path->url('webui') . '"',
+                'path-webui-engine' => '"' . $path->url('webui_engine') . '"',
+                'domain-path' => '""',
+            ),
+        ));
 
         $utility->createResOnAdd($doc, "stylesheet", "default");
         $doc->resources("stylesheet")->chooseOnAdd("default");
